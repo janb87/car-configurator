b9e981f66a305403ef728f7c543f818010550c3f
Jan Bevers <jbevers@sdl.com>
2017-02-21 16:42:55 +0100

    Fixed some lint warnings

--- src/components/CarNumber.js
+++ src/components/CarNumber.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './CarNumber.css';
 
 const CarNumber = (props) => {
--- src/components/Rim.js
+++ src/components/Rim.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Rim.css';
 
 const Rim = (props) => {
