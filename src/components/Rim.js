22002f335c8f80a57489d82001e0b03f23a9598f
Jan Bevers <jbevers@sdl.com>
2017-02-21 16:29:53 +0100

    Use a stateless component were possible

--- src/components/Rim.js
+++ src/components/Rim.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './Rim.css';
 
-class Rim extends Component {
-    render() {
-        const { rimStyle, color } = this.props;
+const Rim = (props) => {
+    const { rimStyle, color } = props;
 
-        return (
-            <div className={`rim ${rimStyle} color-${color}`} />
-        );
-    }
+    return (
+        <div className={`rim ${rimStyle} color-${color}`} />
+    );
 }
 
 export default Rim;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Car from '../components/Car';
 import ConfigurationPanel from './ConfigurationPanel';
 import './App.css';
 
-class App extends Component {
-    render() {
-        const { store } = this.context;
-        const { bodyColor, sideNumber } = store.getState();
+const App = (props, context) => {
+    const { store } = context;
+    const { bodyColor, sideNumber } = store.getState();
 
-        return (
-            <div className="app">
-                <header className="header">
-                    <h2>Welcome to the Car Configuration editor</h2>
-                </header>
-                <main>
-                    <Car bodyColor={bodyColor} sideNumber={sideNumber} />
-                    <ConfigurationPanel />
-                </main>
-            </div>
-        );
-    }
+    return (
+        <div className="app">
+            <header className="header">
+                <h2>Welcome to the Car Configuration editor</h2>
+            </header>
+            <main>
+                <Car bodyColor={bodyColor} sideNumber={sideNumber} />
+                <ConfigurationPanel />
+            </main>
+        </div>
+    );
 }
 
 App.contextTypes = {
